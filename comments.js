02d6230764172706d58b2664b15173e93d4f2e86
kingsleyGE <82558000+KINGSLEYkingsman@users.noreply.github.com>
2023-07-27 23:23:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,25 @@
-// create a web server using express
-// this server will listen to port 3000
-// and will respond to the URL /comments
-// with a json object containing a list of comments
-// this is just a simple example, in reality
-// the comments would be stored in a database
-// and the server would access the database to get the comments
+// create a web server that can respond to requests for comments
+// and send back a list of comments from the server
+// this is a node server
 
-// import the express module
-const express = require('express');
+// require the http module
+var http = require('http');
 
-// create a new express application
-const app = express();
+// create a server
+var server = http.createServer(function(request, response) {
+  // when a request comes in, respond with a 200 status code
+  // and send back a list of comments from the server
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h1>Comments</h1>');
+  response.write('<ul>');
+  response.write('<li>first comment</li>');
+  response.write('<li>second comment</li>');
+  response.write('<li>third comment</li>');
+  response.write('</ul>');
+  response.end();
+});
 
-// create a route for the URL /comments
-// the route will respond to GET requests
-// the route will respond with a json object
-// containing a list of comments
-app.get('/comments', (req, res) => {
-  const comments = [
-    {
-      id: 1,
+// start the server
+server.listen(3000, function() {
+  console.log('Server listening on port 3000');
+});
